e51bba8cdcac7a1bc4239c888734cda84b7e7197
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-04-12 03:23:54 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'myprefix-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<p>below is font awesome icon:</p>
+<i class="fa fa-search"></i> 
+  `,
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'angular-prefix';
+  title = 'angular-prefix';;
 }
